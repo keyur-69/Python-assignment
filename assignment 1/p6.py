f8a751ff17f1fea1461f96b878c454e244082f50
keyur-69 <keyur3887@gmail.com>
2025-09-16 22:16:53 +0530

    Delete assignment 1 directory

--- assignment 1/p6.py
+++
@@ -1,8 +0,0 @@
-username = "Admin" 
-password = "123" 
-uname = input("Enter Username: ") 
-pwd = input("Enter your Password: ") 
-if(uname == username and pwd == password): 
-print("Login Sucessfully!") 
-else: 
-print("Invalid Username or Password")
